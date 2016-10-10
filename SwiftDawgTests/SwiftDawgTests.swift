d9b048aab4f52a59e6b6d408cb6c47d5b3f8b3e4
Maksim Koltsov <developer@tirinox.ru>
2016-10-10 11:24:48 +0300

    delete unused test

--- SwiftDawgTests/SwiftDawgTests.swift
+++ SwiftDawgTests/SwiftDawgTests.swift
@@ -94,11 +94,4 @@ class SwiftDawgTests: XCTestCase {
         XCTAssert(!dictionary(contains: "tremend")) // wants tremendous
     }
     
-//    func testPerformanceExample() {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-    
 }
