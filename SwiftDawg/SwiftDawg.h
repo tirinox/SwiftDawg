fec0851dbb5496aefe1ece17e989cdd088d3f581
tirinox <tirinox@gmail.com>
2016-10-14 14:18:31 +0300

    + util project

--- SwiftDawg/SwiftDawg.h
+++ SwiftDawg/SwiftDawg.h
@@ -5,15 +5,15 @@
 //  Created by Максим Кольцов on 08/10/16.
 //  Copyright © 2016 Maksim Koltsov. All rights reserved.
 //
-
-#import <UIKit/UIKit.h>
-
-//! Project version number for SwiftDawg.
-FOUNDATION_EXPORT double SwiftDawgVersionNumber;
-
-//! Project version string for SwiftDawg.
-FOUNDATION_EXPORT const unsigned char SwiftDawgVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SwiftDawg/PublicHeader.h>
+//
+//#import <UIKit/UIKit.h>
+//
+////! Project version number for SwiftDawg.
+//FOUNDATION_EXPORT double SwiftDawgVersionNumber;
+//
+////! Project version string for SwiftDawg.
+//FOUNDATION_EXPORT const unsigned char SwiftDawgVersionString[];
+//
+//// In this header, you should import all the public headers of your framework using statements like #import <SwiftDawg/PublicHeader.h>
 
 
---
+++ SwiftDawgUtil/SwiftDawgUtil/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  SwiftDawgUtil
+//
+//  Created by Максим Кольцов on 14/10/2016.
+//  Copyright © 2016 Maksim Koltsov. All rights reserved.
+//
+
+import Foundation
+import SwiftDawg
+
+print("Hello, World!")
+
